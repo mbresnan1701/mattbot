1f9f0ec288da8afe766c717ed4f9fe3aef7fa5c1
Matt Bresnan <bresnan.mw@gmail.com>
2016-10-09 12:10:57 -0700

    Authentication setup

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+  public static void main(String... args) throws TwitterException{
+    ConfigurationBuilder cb = new ConfigurationBuilder();
+    cb.setDebugEnabled(true)
+            .setOAuthConsumerKey("rUzpUEF8Yp7YBp4hJ7Gh6xxky")
+            .setOAuthConsumerSecret(System.getenv("consumer_secret"))
+            .setOAuthAccessToken("785150961002041344-jn45ghk2LHgEzRKuigpPoJWh9eBVP13")
+            .setOAuthAccessTokenSecret(System.getenv("token_secret"));
+    TwitterFactory tf = new TwitterFactory(cb.build());
+    Twitter twitter = tf.getInstance();
+
+    //send a tweet
+    Status status = twitter.updateStatus("Have you ever been so that you accidentally the whole thing?");
+
+    //print a message so we know when it finishes
+    System.out.println("Done.");
+  }
 }
